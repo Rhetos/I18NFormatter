7121b85d022a5f507ea5aaaf2788f020555de0e4
Bojan Antolović <bojan.antolovic@gmail.com>
2016-08-08 13:53:46 +0200

    Referencing Rhetos NuGet package instead of local Rhetos dlls. Updated .nuspec for the new Rhetos plugin standard.

--- Build.bat
+++ Build.bat
@@ -1,24 +1,17 @@
 @REM HINT: SET SECOND ARGUMENT TO /NOPAUSE WHEN AUTOMATING THE BUILD.
 
-@SET Config=%1%
-@IF [%1] == [] SET Config=Debug
-
-@IF DEFINED VisualStudioVersion GOTO SkipVcvarsall
-IF "%VS140COMNTOOLS%" NEQ "" CALL "%VS140COMNTOOLS%VsDevCmd.bat" x86 && GOTO EndVcvarsall || GOTO Error0
-IF "%VS120COMNTOOLS%" NEQ "" CALL "%VS120COMNTOOLS%\..\..\VC\vcvarsall.bat" x86 && GOTO EndVcvarsall || GOTO Error0
-IF "%VS110COMNTOOLS%" NEQ "" CALL "%VS110COMNTOOLS%\..\..\VC\vcvarsall.bat" x86 && GOTO EndVcvarsall || GOTO Error0
-IF "%VS100COMNTOOLS%" NEQ "" CALL "%VS100COMNTOOLS%\..\..\VC\vcvarsall.bat" x86 && GOTO EndVcvarsall || GOTO Error0
-ECHO ERROR: Cannot detect Visual Studio, missing VSxxxCOMNTOOLS variable.
-GOTO Error0
-:EndVcvarsall
+CALL "%VS140COMNTOOLS%VsDevCmd.bat" || ECHO ERROR: Cannot find Visual Studio 2015, missing VS140COMNTOOLS variable. && GOTO Error0
 @ECHO ON
-:SkipVcvarsall
 
 PUSHD "%~dp0" || GOTO Error0
-CALL packages\Rhetos\UpdateRhetosDlls.bat /nopause || GOTO Error1
 CALL ChangeVersions.bat || GOTO Error1
-IF EXIST Build.log DEL Build.log || GOTO Error1
-DevEnv.com Rhetos.I18NFormatter.sln /rebuild %Config% /out Build.log || TYPE Build.log && GOTO Error1
+IF EXIST msbuild.log DEL msbuild.log || GOTO Error1
+
+REM NuGet Automatic Package Restore requires "NuGet.exe restore" to be executed before the command-line build.
+WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && GOTO Error1
+NuGet.exe restore Rhetos.I18NFormatter.sln -NonInteractive || GOTO Error1
+
+MSBuild.exe "Rhetos.I18NFormatter.sln" /target:rebuild /p:Configuration=Debug /verbosity:minimal /fileLogger || GOTO Error1
 CALL CreatePackage.bat || GOTO Error1
 CALL ChangeVersions.bat /RESTORE || GOTO Error1
 POPD
--- CreatePackage.bat
+++ CreatePackage.bat
@@ -1,5 +1,4 @@
 PUSHD "%~dp0"
-IF NOT EXIST packages\NugetExe MD packages\NugetExe || POPD && EXIT /B 1
-IF NOT EXIST packages\NugetExe\NuGet.exe PowerShell.exe -Command "Invoke-WebRequest http://nuget.org/nuget.exe -OutFile packages\NugetExe\NuGet.exe" || POPD && EXIT /B 1
-packages\NugetExe\NuGet.exe pack -o .. || POPD && EXIT /B 1
+WHERE /Q NuGet.exe || ECHO ERROR: Please download the NuGet.exe command line tool. && POPD && EXIT /B 1
+NuGet.exe pack -o .. || POPD && EXIT /B 1
 POPD
