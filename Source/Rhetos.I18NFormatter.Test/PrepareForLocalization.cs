733cbbde57b9fda8e8ae490c289c018069957c1c
Bojan Antolović <bojan.antolovic@gmail.com>
2019-07-19 10:24:45 +0200

    Unit tests

---
+++ Source/Rhetos.I18NFormatter.Test/PrepareForLocalization.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhetos.Utilities;
+
+namespace Rhetos.I18NFormatter.Test
+{
+    [TestClass]
+    public class PrepareForLocalizationTest
+    {
+        [TestMethod]
+        public void Simple()
+        {
+            var localizer = new PrepareForLocalization();
+
+            Assert.AreEqual("[[[a%0b%1|||00|||11]]]", localizer["a{0}b{1}", "00", 11]);
+        }
+
+        [TestMethod]
+        public void NullArgs()
+        {
+            var localizer = new PrepareForLocalization();
+
+            Assert.AreEqual("[[[a%0b|||]]]", localizer["a{0}b", new object[] { null }]);
+            Assert.AreEqual("[[[a%0b%1|||0|||]]]", localizer["a{0}b{1}", 0, null]);
+            Assert.AreEqual("[[[a%0b%1||||||]]]", localizer["a{0}b{1}", null, null]);
+        }
+
+        [TestMethod]
+        public void NoArgs()
+        {
+            var localizer = new PrepareForLocalization();
+
+
+            object[] zeroArgs = new object[] { };
+            object[] noArgs = null;
+
+            Assert.AreEqual("[[[ab]]]", localizer["ab", zeroArgs]);
+            Assert.AreEqual("[[[ab]]]", localizer["ab", noArgs]);
+        }
+    }
+}
---
+++ Source/Rhetos.I18NFormatter.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Rhetos.I18NFormatter.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Rhetos.I18NFormatter.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8ca95d1-1043-4d6f-b131-63fd60536738")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
